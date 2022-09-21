53e30e7d275e8e06b60d8c7a4e2763e3175e3f47
nedu-m <davidsonchinedum@gmail.com>
2022-09-21 18:31:24 +0100

    fix: appcontext add to cart

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,16 +1,31 @@
 import { createContext, useState, useEffect } from "react";
 
-//create context
-const AppContext = createContext({
+//create app content and set the types
+const AppContext = createContext<AppContextType>({
   allImages: [],
   toggleFavorite: (imageId: string) => {},
   cartItems: [],
-  addToCart: (imageId: string) => {},
+  addToCart: (newItem: string) => {},
 });
 
-function AppProvider({ children }: { children: React.ReactNode }) {
+//set types for context provider
+interface AppContextProviderProps {
+  children: React.ReactNode;
+}
+//set types for context
+interface AppContextType {
+  allImages: any;
+  toggleFavorite: (imageId: string) => void;
+  cartItems: string[];
+  addToCart: (newItem: string) => void;
+}
+
+function AppProvider({ children }: AppContextProviderProps) {
+  //set state for all images
   const [allImages, setAllImages] = useState([]);
-  const [cartItems, setCartItems] = useState([]);
+
+  //declare cartItems state and set it type to array of strings
+  const [cartItems, setCartItems] = useState<string[]>([]);
 
   function toggleFavorite(imageId: string) {
     setAllImages((prevImages: any) => {
@@ -18,8 +33,6 @@ function AppProvider({ children }: { children: React.ReactNode }) {
       const updatedImages = prevImages.map((image: any) => {
         //if image is found, toggle favorite
         if (image.id === imageId) {
-          console.log(imageId);
-          console.log(!image.isFavorite);
           //return new image with updated favorite
           return { ...image, isFavorite: !image.isFavorite };
         }
@@ -29,17 +42,21 @@ function AppProvider({ children }: { children: React.ReactNode }) {
     });
   }
 
-  function addToCart(imageId: string) {
-    setCartItems((prevItems: any) => {
-      const updatedItems = prevItems.map((item: any) => {
-        if (item.id === imageId) {
-          return { ...item, inCart: !item.inCart };
-        }
-        return item;
-      });
-      return updatedItems;
+  //write addToCart function and add images to cart
+  function addToCart(newItem: string) {
+    setCartItems((prevItems) => {
+      //check if item is already in cart
+      if (prevItems.includes(newItem)) {
+        //if item is already in cart, remove item from cart
+        return prevItems.filter((item) => item !== newItem);
+      } else {
+        //if item is not in cart, add item to cart
+        return [...prevItems, newItem];
+      }
     });
   }
+
+  console.log(cartItems);
 
   /*
   fetch images from api https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json
@@ -53,9 +70,15 @@ function AppProvider({ children }: { children: React.ReactNode }) {
       .then((data) => setAllImages(data));
   }, []);
 
+  //return context provider and set the value types
   return (
     <AppContext.Provider
-      value={{ allImages, toggleFavorite, cartItems, addToCart }}
+      value={{
+        allImages,
+        toggleFavorite,
+        cartItems,
+        addToCart,
+      }}
     >
       {children}
     </AppContext.Provider>
